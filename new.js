fe6e2e51b9312f353a832d5142d8728c52b5808e
gozdeisildak <gozdeisildak@gmail.com>
2019-11-14 15:08:15 +0300

    commit1.1

--- new.js
+++ new.js
@@ -1,21 +1,21 @@
-select * from `order` o 
-inner join orderDetails od on o.id=od.order_id 
-where o.id in (select od2.order_id from orderDetails od2 where od2.type='sandalye');
+select * from`order` o
+inner join orderDetails od on o.id = od.order_id
+where o.id in (select od2.order_id from orderDetails od2 where od2.type = 'sandalye');
 show profiles
-
+çslakdsldşadşasaaassak
 
 
 const q2 = (req, res) => {
-    order.findAll({
-      include:[{
+  order.findAll({
+    include: [{
+      model: orderDetail,
+      include: [{
         model: orderDetail,
-        include:[{
-          model:orderDetail,
-          where: {type:'sandalye'}
-        }],
-        required: true
-      }]
-    })
+        where: { type: 'sandalye' }
+      }],
+      required: true
+    }]
+  })
     .then(data => {
       return res.send({
         message: 'Sandalyesi olan  butun orderlar',
@@ -24,8 +24,8 @@ const q2 = (req, res) => {
     }).catch(error => {
       return res.send('Hata Sandalyesi olan  butun orderlar: ', error)
     })
-  }
-  orderDetail.hasMany(orderDetail, {
-    foreignKey: 'order_id',
-    sourceKey: 'order_id'
-  });
+}
+orderDetail.hasMany(orderDetail, {
+  foreignKey: 'order_id',
+  sourceKey: 'order_id'
+});
